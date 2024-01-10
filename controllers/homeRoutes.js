e9b1d7902bae493fd28167ff93939c0f40ec068e
Matt R <mattrallo2012@gmail.com>
2024-01-09 18:35:50 -0600

    navbar and all pages are accessible

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,4 +8,17 @@ router.get('/', async (req, res) => {
   
 });
 
+router.get('/dashboard', (req, res) => {
+  
+    res.render('dashboard');
+});
+
+
+
+router.get('/login', (req, res) => {
+  
+    res.render('login');
+});
+
+
 module.exports = router;
