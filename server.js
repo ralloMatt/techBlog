fb3e54e6feaf4962af7e5e5ce12de9a981925dc8
Matt R <mattrallo2012@gmail.com>
2024-01-16 23:36:31 -0600

    time displays nicer

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars'); // for my html pages which are handlebars
+const helpers = require('./utils/helpers'); // helper functions (used for time display)
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -10,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001; // allows for deployment on heroku and on localhost
 
-const hbs = exphbs.create(); // handle bar engine
+const hbs = exphbs.create({ helpers }); // handle bar engine (set up engine with custion helpers)
 
 const sess = {
     secret: 'Shhhhhhh it is a secret....',
